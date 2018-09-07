b4d8c25461eb0dc09db3c92e7af912d3c348427c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-07 17:33:40 -0400

    W7D5 exercise

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Greeting from './greeting_container';
+
+const App = () => (
+  <div>
+    <h1>Bench BnB</h1>
+    <Greeting />
+  </div>
+);
+
+export default App;
